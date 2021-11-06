140574e4a59cf2d92c33c1ffada6b972970d3bb4
BRYAN STEVEN PEREZ BARRIOS <bryan.perez@est.uca.edu.ni>
2021-11-06 02:20:03 -0600

    chore: remove git add command from lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.+(js|jsx|ts|tsx)': ['prettier --write', 'eslint --fix', 'git add'],
-  '*.+(json|yml|yaml|css|less|scss|md|graphql|mdx)': ['prettier --write', 'git add'],
+  '*.+(js|jsx|ts|tsx)': ['prettier --write', 'eslint --fix'],
+  '*.+(json|yml|yaml|css|less|scss|md|graphql|mdx)': ['prettier --write'],
 };
